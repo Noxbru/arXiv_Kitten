6b838cd43f436954e935c74226bcbd2181a4f3f6
Bruno Jiménez <brunojimen@gmail.com>
2017-03-03 23:23:13 +0000

    Basic support for adding filters

--- src/user.py
+++ src/user.py
@@ -2,11 +2,15 @@ class User:
     def __init__(self, id, name):
         self.id = id
         self.name = name
-        self.feeds = set()
+        self.feeds = {}
+        self.last_feed_added = None
 
     def add_feed(self, feed_name):
-        self.feeds.add(feed_name)
+        self.feeds[feed_name] = []
         self.last_feed_added = feed_name
 
     def add_filter(self, filter):
-        pass
+        if self.last_feed_added == None:
+            return None
+
+        self.feeds[self.last_feed_added] += [filter]
